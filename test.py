9ae0b69823c77a50852d61fbdffd8d63b75b2842
Ondrej Certik <ondrej@certik.cz>
2009-06-29 17:36:08 -0600

    Use C stuff

--- test.py
+++ test.py
@@ -1,12 +1,10 @@
-from pyglet.gl import (glClear, glLoadIdentity, glBegin, glVertex2f, glEnd,
-        GL_COLOR_BUFFER_BIT, GL_TRIANGLES)
-
 import pyglet
+import pyglet.gl
 import osmesa
 
 window = pyglet.window.Window()
-label = pyglet.text.Label('Hello, world', 
-                          font_name='Times New Roman', 
+label = pyglet.text.Label('Hello, world',
+                          font_name='Times New Roman',
                           font_size=36,
                           x=window.width//2, y=window.height//2,
                           anchor_x='center', anchor_y='center')
@@ -14,18 +12,7 @@ label = pyglet.text.Label('Hello, world',
 @window.event
 def on_draw():
     window.clear()
-    # this segfaults:
-    #osmesa.render()
-    #pyglet.graphics.draw(2, pyglet.gl.GL_POINTS,
-    #            ('v2i', (10, 15, 30, 35))
-    #            )
-    glClear(GL_COLOR_BUFFER_BIT)
-    glLoadIdentity()
-    glBegin(GL_TRIANGLES)
-    glVertex2f(0, 0)
-    glVertex2f(window.width, 0)
-    glVertex2f(window.width, window.height)
-    glEnd()
+    osmesa.render()
     label.draw()
 
 pyglet.app.run()
