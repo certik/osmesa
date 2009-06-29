e97ecb951f9000ea54b1f588f3c11350f239db21
Ondrej Certik <ondrej@certik.cz>
2009-06-29 17:41:25 -0600

    draw some triangles

--- test.py
+++ test.py
@@ -14,5 +14,12 @@ def on_draw():
     window.clear()
     osmesa.render()
     label.draw()
+    pyglet.graphics.draw_indexed(4, pyglet.gl.GL_TRIANGLES,
+        [0, 1, 2, 0, 2, 3],
+        ('v2i', (100, 100,
+                 150, 100,
+                 150, 150,
+                 100, 150))
+    )
 
 pyglet.app.run()
