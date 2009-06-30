0367cc53fd0fd83e25be61c12448e92287601c66
Ondrej Certik <ondrej@certik.cz>
2009-06-29 23:34:41 -0600

    Read more

--- gengl.py
+++ gengl.py
@@ -4,14 +4,13 @@ from pyparsing import (Word, Combine, Optional, alphas, alphanums, oneOf,
 testdata = open("/usr/include/GL/gl.h").read()
 
 ident = Word(alphas, alphanums + "_")
-vartype = Combine( oneOf("float double int GLdouble") + Optional(Word("*")),
-        adjacent = False)
+vartype = Combine( ident + Optional(Word("*")), adjacent = False)
 arglist = delimitedList( Group(vartype.setResultsName("type") + \
         ident.setResultsName("name")) )
 functionCall = ident.setResultsName("name") + \
         "(" + arglist.setResultsName("args") + ")" + ";"
 
-for fn,s,e in functionCall.scanString(testdata):
+for fn, s, e in functionCall.scanString(testdata):
     print fn.name, fn.return_type
     for a in fn.args:
         print " -", a.type, a.name
