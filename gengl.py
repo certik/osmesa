a007210cf1ff0819ade279b62142292b2058c6db
Ondrej Certik <ondrej@certik.cz>
2009-06-29 23:27:16 -0600

    It reads something

--- gengl.py
+++ gengl.py
@@ -1,17 +1,17 @@
-from pyparsing import *
+from pyparsing import (Word, Combine, Optional, alphas, alphanums, oneOf,
+        delimitedList, Group)
 
-testdata = """
-int func1(float *arr, int len, double arg1);
-int func2(float **arr, float *arr2, int len, double arg1, double arg2);
-"""
+testdata = open("/usr/include/GL/gl.h").read()
 
 ident = Word(alphas, alphanums + "_")
-vartype = Combine( oneOf("float double int") + Optional(Word("*")), adjacent = False)
-arglist = delimitedList( Group(vartype.setResultsName("type") + ident.setResultsName("name")) )
-functionCall = Literal("int") + ident.setResultsName("name") + \
-    "(" + arglist.setResultsName("args") + ")" + ";"
+vartype = Combine( oneOf("float double int GLdouble") + Optional(Word("*")),
+        adjacent = False)
+arglist = delimitedList( Group(vartype.setResultsName("type") + \
+        ident.setResultsName("name")) )
+functionCall = ident.setResultsName("name") + \
+        "(" + arglist.setResultsName("args") + ")" + ";"
 
 for fn,s,e in functionCall.scanString(testdata):
-    print fn.name
+    print fn.name, fn.return_type
     for a in fn.args:
         print " -", a.type, a.name
