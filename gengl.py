af62617b1781b6ced981b18b2fa37879cc20ae58
Ondrej Certik <ondrej@certik.cz>
2009-06-30 00:04:06 -0600

    Print definitions

--- gengl.py
+++ gengl.py
@@ -3,17 +3,22 @@
 from pyparsing import (Word, Combine, Optional, alphas, alphanums, oneOf,
         delimitedList, Group)
 
-testdata = open("/usr/include/GL/gl.h").read()
+header = "/usr/include/GL/gl.h"
+testdata = open(header).read()
 
 ident = Word(alphas, alphanums + "_")
-vartype = (Optional(Word("const")) + Combine(ident + Optional(Word("*")),
+vartype = (Optional("const") + Combine(ident + Optional(Word("*")),
         adjacent = False))
 arglist = delimitedList( Group(vartype.setResultsName("type") + \
         ident.setResultsName("name")) )
 functionCall = ident.setResultsName("name") + \
-        "(" + (arglist.setResultsName("args") | Word("void")) + ")" + ";"
+        "(" + (arglist.setResultsName("args") | "void") + ")" + ";"
+typedef = "typedef" + Optional("unsigned") + oneOf("int char short float void") + ident + ";"
 
-for fn, s, e in functionCall.scanString(testdata):
-    print fn.name, fn.return_type
-    for a in fn.args:
-        print " -", a.type, a.name
+print """cdef extern from "%s":""" % (header)
+for fn, s, e in typedef.scanString(testdata):
+    print "    ctypedef", " ".join(fn[1:-1])
+#for fn, s, e in functionCall.scanString(testdata):
+#    print fn.name, fn.return_type
+#    for a in fn.args:
+#        print " -", a.type, a.name
