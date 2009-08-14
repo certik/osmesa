ee2221a8c77a091deb468d665b8eb5c8e4118c69
Ondrej Certik <ondrej@certik.cz>
2009-08-13 22:17:03 -0600

    ok

--- osmesa.pyx
+++ osmesa.pyx
@@ -7,22 +7,6 @@ cdef extern void free_context()
 cdef extern void sph(double angle)
 cdef extern void Sphere(float radius, int slices, int stacks)
 
-cdef extern from "/usr/include/GL/gl.h":
-    ctypedef float GLfloat
-    void c_glVertex3f "glVertex3f"(GLfloat x, GLfloat y, GLfloat z)
-
-def glVertex3f(x, y, z):
-    c_glVertex3f(x, y, z)
-
-def render():
-    render_image()
-
-def render2():
-    render_image2()
-
-def render3():
-    render_image3()
-
 def init_ctx(int width, int height):
     r = init_context(width, height)
     if r == -1:
@@ -30,9 +14,3 @@ def init_ctx(int width, int height):
 
 def free_ctx():
     free_context()
-
-def sphere(angle):
-    sph(angle)
-
-def sphere2(float radius, int slices, int stacks):
-    Sphere(radius, slices, stacks)
