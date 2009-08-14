babc5f950cc596b7daa8c0102ed33f548e96129a
Ondrej Certik <ondrej@certik.cz>
2009-08-13 22:17:20 -0600

    ok

--- osmesa.pyx
+++ osmesa.pyx
@@ -1,11 +1,5 @@
-cdef extern void render_image()
-cdef extern void render_image2()
-cdef extern void render_image3()
 cdef extern int init_context(int w, int h)
 cdef extern void free_context()
-
-cdef extern void sph(double angle)
-cdef extern void Sphere(float radius, int slices, int stacks)
 
 def init_ctx(int width, int height):
     r = init_context(width, height)
